41d16e3c6102a78ee611889df70443bdf676ff72
jip1080 <jp.mcconnell@gmail.com>
2016-11-19 18:27:30 -0500

    add do_move on board, specs to validate

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ActiveRecord::Base
+  class InvalidMoveError < StandardError; end
 
   belongs_to :game
 
@@ -19,5 +20,22 @@ class Board < ActiveRecord::Base
         open_columns << index if col.match(/^0.*/).present?
       end
     end
+  end
+
+  def do_move(player_number, play_hash)
+    col = play_hash[:col]
+    validate_move(col)
+    col = col.to_i
+    row_index = board[col].rindex("0")
+    board[col][row_index] = player_number.to_s
+    self.save
+  end
+
+  def validate_move(column)
+    fail InvalidMoveError unless column.present?                          # must be present
+    fail InvalidMoveError unless column.to_i.to_s == column.to_s          # must be numeric
+    fail InvalidMoveError unless column.to_i == column.to_f               # must be an integer
+    fail InvalidMoveError unless available_columns.include?(column.to_i)  # must be available to play
+    true
   end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -33,4 +33,46 @@ describe Board do
       expect(board.available_columns).to match_array([0,3])
     end
   end
+
+  context '#do_move' do
+    let(:starting_board) { ["0000", "0001", "0011", "1111"] }
+    let(:board) { FactoryGirl.create(:board, rows: 4, columns: 4) }
+
+    before(:each) do
+      board.board = starting_board
+      board.save!
+    end
+
+    it 'updates the board with a valid move' do
+      after_move_board = ["0001", "0001", "0011", "1111"]
+      expect(board.board).to match_array(starting_board)
+      board.do_move("1", {col: "0"})
+      board.reload
+      expect(board.board).to match_array(after_move_board)
+    end
+
+    it 'places the token into the first available slot' do
+      after_move_board = ["0000", "0011", "0011", "1111"]
+      expect(board.board).to match_array(starting_board)
+      board.do_move("1", {col: "1"})
+      board.reload
+      expect(board.board).to match_array(after_move_board)
+    end
+
+    it 'raises an error if the column is full' do
+      expect { board.do_move(1, {col: "3"}) }.to raise_error Board::InvalidMoveError
+    end
+
+    it 'raises an error if the column does not exist' do
+      expect { board.do_move(1, {col: "10"}) }.to raise_error Board::InvalidMoveError
+    end
+
+    it 'raises an error if the move is not an integer' do
+      expect { board.do_move(1, {col: "10.1"}) }.to raise_error Board::InvalidMoveError
+    end
+
+    it 'raises an error if the move is not numeric' do
+      expect { board.do_move(1, {col: "cat"}) }.to raise_error Board::InvalidMoveError
+    end
+  end
 end
