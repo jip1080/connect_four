127d6a59e9a4cec89448f2cda8a5b950786ea9c4
jip1080 <jp.mcconnell@gmail.com>
2016-11-19 17:05:54 -0500

    #6 add available columns to play algorithm

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,11 +5,19 @@ class Board < ActiveRecord::Base
   validates :rows,    numericality: { only_integer: true, greater_than: 0 }
   validates :columns, numericality: { only_integer: true, greater_than: 0 }
 
-  before_save :initialize_board
+  before_create :initialize_board
 
   def initialize_board
     self.board = [].tap do |new_board|
       columns.times { new_board << ("0" * rows) }
+    end
+  end
+
+  def available_columns
+    [].tap do |open_columns|
+      board.each_with_index do |col, index|
+        open_columns << index if col.match(/^0.*/).present?
+      end
     end
   end
 end
